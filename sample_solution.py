5dd4a81eb1b9b7b7a9cde8f6cb284b576def28ee
Squire, Geoffrey (Data61, Black Mountain) <geoffrey.squire@data61.csiro.au>
2021-03-24 13:27:48 +1100

    Update to use the cropA sample from pyrate 0.6

--- sample_solution.py
+++ sample_solution.py
@@ -3,157 +3,217 @@ import os.path
 import subprocess
 import sys
 from tempfile import TemporaryDirectory
+from zipfile import ZipFile
 
 CONF_FILE = r"""
 # PyRate configuration file for GAMMA-format interferograms
 #
-#------------------------------------
-# input/output parameters
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Optional ON/OFF switches - ON = 1; OFF = 0
 
-# Directory for the (unwrapped) interferograms.
-obsdir:       /g/data/dg9/insar/Data61_VL/toolbox/PyRate/sample_data/input/interferograms
+# Coherence masking (PREPIFG)
+cohmask:       0
 
+# Orbital error correction (CORRECT)
+orbfit:        1
+
+# APS correction using spatio-temporal filter (CORRECT)
+apsest:        1
+
+# DEM error (residual topography) correction (CORRECT)
+demerror:      1
+
+# Phase Closure correction (CORRECT)
+phase_closure:      1
+
+# Optional save of numpy array files for output products (MERGE)
+savenpy:       1
+
+# Optional save of incremental time series products (TIMESERIES/MERGE)
+savetsincr:    1
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Multi-threading parameters used by correct/stacking/timeseries
+# gamma prepifg runs in parallel on a single machine if parallel = 1
+# parallel: 1 = parallel, 0 = serial
+parallel:      0
+# number of processes
+processes:     4
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Input/Output file locations
+#
 # File containing the list of interferograms to use.
-ifgfilelist:  /g/data/dg9/insar/Data61_VL/toolbox/PyRate/sample_data/input/interferogram_list.txt
+ifgfilelist:   tests/test_data/cropA/ifg_30
 
 # The DEM file used in the InSAR processing
-demfile:      /g/data/dg9/insar/Data61_VL/toolbox/PyRate/sample_data/input/dem/20060619_utm.dem
+demfile:       tests/test_data/cropA/geotiffs/cropA_T005A_dem.tif
 
 # The DEM header file from GAMMA (*.par) or ROI_PAC (*.rsc).
-demHeaderFile: /g/data/dg9/insar/Data61_VL/toolbox/PyRate/sample_data/input/dem/20060619_utm_dem.par
+demHeaderFile: tests/test_data/cropA/headers/cropA_20180106_VV_8rlks_eqa_dem.par
 
-# GAMMA only: The directory containing GAMMA slc.par header files for all epochs
-slcFileDir:   /g/data/dg9/insar/Data61_VL/toolbox/PyRate/sample_data/input/headers
-
-# GAMMA only: File listing the pool of available slc.par header files
-slcfilelist: /g/data/dg9/insar/Data61_VL/toolbox/PyRate/sample_data/input/headers_list.txt
-
-# Directory containing the coherence files. If not provided, obsdir will be used.
-cohfiledir:
+# File listing the pool of available header files (GAMMA: *mli.par, ROI_PAC: *.rsc)
+hdrfilelist:   tests/test_data/cropA/headers_13
 
 # File listing the pool of available coherence files.
-cohfilelist:
+cohfilelist:   tests/test_data/cropA/coherence_30
+
+# File listing the pool of available baseline files (GAMMA).
+basefilelist:  tests/test_data/cropA/baseline_30
+
+# Look-up table containing radar-coded row and column for lat/lon pixels (GAMMA)
+ltfile:        tests/test_data/cropA/geometry/20180106_VV_8rlks_eqa_to_rdc.lt
 
 # Directory to write the outputs to
-outdir:       {0}
+outdir:        {0}
 
-# InSAR processing software: ROI_PAC = 0, GAMMA = 1
-processor:    1
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# PREPIFG parameters
+#------------------------------------
+# Input data format: ROI_PAC = 0, GAMMA = 1
+processor:     1
 
-# No data averaging threshold for prepifg
+# Coherence threshold value for masking, between 0 and 1
+cohthresh:     0.05
+
+# Multi-look/subsampling factor in east (x) and north (y) dimension
+ifglksx:       1
+ifglksy:       1
+
+# Cropping options
+# ifgcropopt: 1 = minimum extent 2 = maximum extent 3 = crop 4 = no cropping
+# ifgxfirst,ifgyfirst: longitude (x) and latitude (y) of north-west corner
+# ifgxlast,ifgylast: longitude (x) and latitude (y) of south-east corner
+ifgcropopt:    1
+ifgxfirst:     150.92
+ifgyfirst:     -34.18
+ifgxlast:      150.94
+ifgylast:      -34.22
+
+# No-data averaging threshold (0 = 0%; 1 = 100%)
 noDataAveragingThreshold: 0.5
 
-# The no data value in the interferograms
-noDataValue:  0.0
+# The No-data value used in the interferogram files
+noDataValue:   0.0
 
-# Nan conversion flag. Set to 1 if missing (0) phase values are converted to nan
+# Nan conversion flag. Set to 1 if missing No-data values are to be converted to NaN
 nan_conversion: 1
 
-#-----------------------------------
-# Multi-threading parameters: used by linrate/timeseries/prepifg
-# gamma prepifg runs in parallel on a single machine if parallel != 0
-# parallel = 1, linrate/timeseries computation is done in parallel by rows
-# parallel = 2, linrate/timeseries computation is done in parallel for each pixel
-# parallel = 0, linrate/timeseries computation is done in serial pixel by pixel
-parallel:  0
-processes: 1
-
-#------------------------------------
-# Coherence masking options: used by process
-# cohmask: 1 = ON, 0 = OFF
-# cohthresh: coherence threshold value, between 0 and 1
-cohmask:   0
-cohthresh:  0.1
-
-#------------------------------------
-# Interferogram multi-look and crop options
-# ifgcropopt: 1 = minimum 2 = maximum 3 = customise 4 = all ifms already same size
-# ifglksx/y: multi-look/subsampling factor in east and north direction respectively
-# ifgxfirst,ifgyfirst: x,y of top-left corner
-# ifgxlast,ifgylast: x,y of bottom-right corner
-ifgcropopt:   4
-ifglksx:      1
-ifglksy:      1
-ifgxfirst:    150.92
-ifgxlast:     150.94
-ifgyfirst:    -34.18
-ifgylast:     -34.22
-
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# CORRECT parameters
 #------------------------------------
 # Reference pixel search options
+
 # refx/y: Lon/Lat coordinate of reference pixel. If left blank then search for best pixel will be performed
-# refnx/y: number of search grid points in x/y direction
-# refchipsize: chip size of the data window at each search grid point
+# refnx/y: number of search grid points in x/y image dimensions
+# refchipsize: size of the data window at each search grid point
 # refminfrac: minimum fraction of valid (non-NaN) pixels in the data window
-refx:          
-refy:          
+refx:          -1
+refy:          -1
 refnx:         5
 refny:         5
 refchipsize:   5
-refminfrac:    0.01
+refminfrac:    0.8
 
 #------------------------------------
-# Reference phase calculation method
+# Reference phase correction method
+
 # refest: 1 = median of the whole interferogram
 # refest: 2 = median within the window surrounding the chosen reference pixel
-refest:        2
+refest:        1
 
 #------------------------------------
 # Orbital error correction
-# orbfit: ON = 1, OFF = 0
-# orbfitmethod = 1: independent method; 2: network method
+
+# orbfitmethod = 1: interferograms corrected independently; 2: network method
 # orbfitdegrees: Degree of polynomial surface to fit (1 = planar; 2 = quadratic; 3 = part-cubic)
-# orbfitlksx/y: additional multi-look factor for orbital correction
-orbfit:        1
+# orbfitlksx/y: additional multi-look factor for network orbital correction
 orbfitmethod:  1
 orbfitdegrees: 1
 orbfitlksx:    1
 orbfitlksy:    1
 
-#------------------------------------
-# APS correction using spatio-temporal filter
-# apsest: ON = 1, OFF = 0
-# Spatial low-pass filter parameters
-# slpfmethod: filter method (1: butterworth; 2: gaussian)
-# slpfcutoff: cutoff d0 (greater than zero) in km for both butterworth and gaussian filters
-# slpforder: order n for butterworth filter (default 1)
-# slpnanfill: 1 for interpolation, 0 for zero fill
-# slpnanfill_method: linear, nearest, cubic; only used when slpnanfill=1
-# Temporal low-pass filter parameters
-# tlpfmethod: 1 = Gaussian, 2 = Triangular, 3 = Mean filter
-# tlpfcutoff: cutoff t0 for gaussian filter in year;
-# tlpfpthr: valid pixel threshold;
-apsest:         0
-slpfmethod:     2
-slpfcutoff:     0.001
-slpforder:      1
-slpnanfill:     1
-slpnanfill_method:  cubic
-tlpfmethod:   3
-tlpfcutoff:   0.25
-tlpfpthr:     1
+# phase closure params
+# large_deviation_threshold_for_pixel # multiples of pi. For ex: 0.5 means pi/2, 1 means pi etc.
+# avg_ifg_err_thr: ifgs with more than this fraction of pixels with error will be dropped
+# loops_thr_ifg: pixel with phase unwrap error in at least this many loops
+# phs_unw_err_thr: pixel with phase unwrap error in more than this many ifgs will be flagged
+# max_loop_length: loops upto this many edges are considered for closure checks
+# subtract_median: whether to subtract median during closure checks
+# max_loops_in_ifg: loops with more than these many ifgs are discarded.
+# max_loops_in_ifg: Ifg count must be met for all ifgs in the loop for loop to be discarded
+large_dev_thr: 0.5
+avg_ifg_err_thr: 0.07
+loops_thr_ifg: 2
+phs_unw_err_thr: 5
+max_loop_length: 4
+subtract_median: 1
+max_loops_in_ifg: 2
+
 
 #------------------------------------
-# Time Series Calculation
-# tscal: YES = 1, NO = 0
+# APS filter parameters
+
+# tlpfcutoff: cutoff t0 for temporal high-pass Gaussian filter in days (int);
+# tlpfpthr: valid pixel threshold;
+# slpfcutoff: spatial low-pass Gaussian filter cutoff in km (greater than zero).
+#             slpfcutoff=0 triggers cutoff estimation from exponential covariance function
+tlpfcutoff:    30
+tlpfpthr:      1
+slpfcutoff:    1
+
+#------------------------------------
+# DEM error (residual topography) correction parameters
+
+# de_pthr: valid observations threshold;
+de_pthr:       20
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# TIMESERIES parameters
+#------------------------------------
+
 # tsmethod: Method for time series inversion (1 = Laplacian Smoothing; 2 = SVD)
-# smorder: order of Laplacian smoothing operator (1 =  first-order difference; 2 = second-order difference)
+# smorder: order of Laplacian smoothing operator (1 = first-order difference; 2 = second-order difference)
 # smfactor: smoothing factor for Laplacian smoothing (value provided is converted as 10**smfactor)
 # ts_pthr: valid observations threshold for time series inversion
-tscal:         1
 tsmethod:      2
 smorder:       2
-smfactor:     -0.25
+smfactor:      -0.25
 ts_pthr:       10
 
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# STACK parameters
 #------------------------------------
-# Stacking calculation
-# pthr: minimum number of coherent ifg connections for each pixel
-# nsig: n-sigma used as residuals threshold for iterativel least squares stacking
-# maxsig: maximum residual used as a threshold for values in the rate map
-nsig:          3
+
+# pthr: threshold for minimum number of ifg observations for each pixel
+# nsig: threshold for iterative removal of observations
+# maxsig: maximum sigma (std dev) used as an output masking threshold applied in Merge step. 0 = OFF.
 pthr:          5
-maxsig:        2
+nsig:          3
+maxsig:        100
+
+# LOS Projection of timeseries and stack products
+# converts slanted (native) LOS signals to either "vertical" or "horizontal", by multiplying by the sine or cosine of
+# the incidence angle for each pixel.
+# Three options, 0/1/2 - 0 = LOS (no conversion); 1 = pseudo-vertical; 2 = pseudo-horizontal.
+
+los_projection: 0
+
+# optionally supply rows and cols for tiles used during correct and merge step
+rows:          4
+cols:          3
+
+largetifs:     0
+
+[correct]
+steps =
+    orbfit
+    refphase
+    demerror
+    phase_closure
+    mst
+    apscorrect
+    maxvar
 """
 
 def run_pyrate_cmd(pyrate_cmd, config_file, *args):
@@ -172,12 +232,9 @@ with TemporaryDirectory() as temp_output_dir:
     with open(conf_file, 'w') as f:
         f.write(CONF_FILE.format(temp_output_dir))
 
-    # Run PyRate
+    # Run complete PyRate workflow
     try:
-        run_pyrate_cmd("conv2tif", conf_file)
-        run_pyrate_cmd("prepifg", conf_file)
-        run_pyrate_cmd("process", conf_file)
-        run_pyrate_cmd("merge", conf_file)
+        run_pyrate_cmd("workflow", conf_file)
     except subprocess.CalledProcessError as ex:
         with open(os.path.join(temp_output_dir, "EXCEPTION.txt"), 'w') as f:
             f.write(f"PyRate job failed with exception.\nreturncode: {ex.returncode}\nfailed command: {ex.cmd}\n\nexception: {ex!r}")
@@ -185,6 +242,12 @@ with TemporaryDirectory() as temp_output_dir:
     # Work around "cloud" bug in vgl by running in the output dir so we can use
     # relative filenames.
     os.chdir(temp_output_dir)
+    # Zip directories for cloud upload
+    for f in os.listdir(temp_output_dir):
+        abs_f = os.path.join(temp_output_dir, f)
+        if os.path.isdir(abs_f):
+            with ZipFile(f"{f}.zip", 'a') as z:
+                z.write(f)
     # Upload results, files only
     for f in os.listdir(temp_output_dir):
         abs_f = os.path.join(temp_output_dir, f)
